0801ae26dd0a3029bc916083b8ed3373d3678902
Mohamed Abobakr <mabobakr277@gmail.com>
2026-03-09 17:28:06 +0200

    Added Task Day4

---
+++ TasksSolution/LINQ&EF/TaskDay04/ECommerceSystemSolution/ECommerceSystem/DbContexts/ECommerceDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceSystem.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceSystem.DbContexts
+{
+    internal class ECommerceDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=ECommerceDB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new { od.OrderId, od.ProductId });
+        }
+    }
+}
---
+++ TasksSolution/LINQ&EF/TaskDay04/LibrarySystemSolution/LibrarySystem/DbContexts/LibrarySystemDbContext.cs
@@ -0,0 +1,33 @@
+﻿using LibrarySystem.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.DbContexts
+{
+    internal class LibrarySystemDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Borrower> Borrowers { get; set; }
+
+        public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=LibraryDB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Loan>()
+                .HasKey(l => new { l.BookId, l.BorrowerId });
+        }
+    }
+}
